82e36e2bbc134c1cb5ef8191d37018468342f806
B20DCAT186TranDangThuc <trthucpro@gmail.com>
2024-06-02 14:54:49 +0700

    before custom security

--- src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
@@ -1,14 +1,21 @@
 package vn.hoidanit.laptopshop.domain.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import vn.hoidanit.laptopshop.service.validator.RegisterChecked;
 
 @RegisterChecked
 public class RegisterDTO {
 
+    @Size(min = 3, message = "firstName phải có ít nhất 3 ký tự")
     private String firstName;
     private String lastName;
+
+    @Email(message = "Email không hợp lệ", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     private String email;
     private String password;
+
+    @Size(min = 3, message = "confirmPassword phải có ít nhất 3 ký tự")
     private String confirmPassword;
 
     public String getFirstName() {
